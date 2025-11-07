7e4aec1cce9bb2ed336842a51c8699a1d6c20bba
Oscuryo <139709832+Oscuryo@users.noreply.github.com>
2025-11-07 19:01:04 +0100

    first

---
+++ Actividad 1/js.js
@@ -0,0 +1,9 @@
+function autor(){
+    
+    Swal.fire({
+        title: 'DAVID RAMIREZ GRIÑO',
+        text: 'Desarrollado por: ',
+        icon: 'success',
+        confirmButtonText: 'OK'
+});
+}
